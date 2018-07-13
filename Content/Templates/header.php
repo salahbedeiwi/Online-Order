8533983ab359a3d30f24b34f86049968cc549df6
Salah Bedeiwi <12550747+salahbedeiwi@users.noreply.github.com>
2018-07-12 22:19:04 -0500

    updated header
    updated header

--- Content/Templates/header.php
+++ Content/Templates/header.php
@@ -1,5 +1,13 @@
 <div class="container">
-	<!-- Add Order Total, Login Name-->
-	<p><span class="glyphicon glyphicon-shopping-cart pull-left"></span>$<span id='showTotalOnHeader'>0.00</span><span class="logInName pull-right">Salah Bedeiwi</span><span class="logInId hide">153265</span></p>
+	<!-- Add Order Total, Login Name, Login/Logout Btns-->
+	<p>
+			<span class="glyphicon glyphicon-shopping-cart pull-left"></span>
+			$<span id='showTotalOnHeader'>0.00</span>
+			<span class="logInOrLogOut pull-right">
+				&nbsp;<span class="glyphicon glyphicon-log-in"></span>&nbsp;
+			</span>
+			<span class="logInName pull-right"></span>&nbsp;
+			<span class="logInId hide"></span>
+	</p>
 </div>
 <div class="clearfix"></div>
